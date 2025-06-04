b5b58d9df35d820168bd8150f6c17aa7d7bbe61b
vrnprkh <paintingburritos@gmail.com>
2025-06-04 19:01:47 -0400

    fixed sidebar toggle properties

--- src/components/SidebarToggle.tsx
+++ src/components/SidebarToggle.tsx
@@ -12,9 +12,9 @@ export default function SidebarToggle() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="w-6"
         >
           <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
